67e758022e8fcc2ca373551c063821272a37095d
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-08-10 04:17:27 +0300

    feat: implement email verification with otp notification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendOtpMail;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -21,32 +25,23 @@ class RegisteredUserController extends Controller
     {
         $data = $request->validated();
 
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+        $data['password'] = Hash::make($data['password']);
 
-            'phone' => $data['phone'],
-            'blood_type' => $data['blood_type'],
+        unset($data['password_confirmation']);
 
-            'latitude' => $data['address']['latitude'],
-            'longitude' => $data['address']['longitude'],
-            'address_text' => $data['address']['address_text'],
-        ]);
+        $otp = str_pad(random_int(0, 9999), 4, '0', STR_PAD_LEFT);
 
-        event(new Registered($user));
+        $verificationId = Str::uuid()->toString();
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $cacheKey = 'registration_' . $verificationId;
+
+        Cache::put($cacheKey, ['userdata' => $data, 'otp'  => Hash::make($otp)], now()->addMinutes(3));
+
+        Mail::to($data['email'])->send(new SendOtpMail($otp));
 
         return response()->json([
-            'message' => 'Registration successful',
-            'user' => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-                'blood_type' => $user->blood_type,
-            ],
-            'token' => $token,
-        ], 201);
+            'message' => 'OTP sent to your email. Please verify to complete registration.',
+            'verification_id' => $verificationId,
+        ], 200);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 #[Fillable([
     'name',
@@ -19,10 +20,11 @@ use Laravel\Sanctum\HasApiTokens;
     'blood_type',
     'latitude',
     'longitude',
-    'address_text'
+    'address_text',
+    'email_verified_at',
 ])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
