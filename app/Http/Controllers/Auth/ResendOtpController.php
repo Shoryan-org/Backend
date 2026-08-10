b69b4f3535b3cbda8bfe35801da74887dc59ab5b
Hatem Ayman <hatem.ayman.508@gmail.com>
2026-08-10 04:55:01 +0300

    feat: implement resending otp

---
+++ app/Http/Controllers/Auth/ResendOtpController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ResendOtpRequest;
+use App\Mail\SendOtpMail;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+
+class ResendOtpController extends Controller
+{
+    public function __invoke(ResendOtpRequest $request): JsonResponse
+    {
+        $verificationId = $request->verification_id;
+
+        $cacheKey = 'registration_' . $verificationId;
+
+        $cachedData = Cache::get($cacheKey);
+
+        if (! $cachedData) {
+            return response()->json([
+                'message' => 'Your session has expired. Please try again.',
+            ], 400);
+        }
+
+        $otp = str_pad(random_int(0, 9999), 4, '0', STR_PAD_LEFT);
+
+        $cachedData['otp'] = Hash::make($otp);
+
+        Cache::put(
+            $cacheKey,
+            $cachedData,
+            now()->addMinutes(3)
+        );
+
+        Mail::to($cachedData['userdata']['email'])
+            ->send(new SendOtpMail($otp));
+
+        return response()->json([
+            'message' => 'A new OTP has been sent to your email.',
+        ], 200);
+    }
+}
