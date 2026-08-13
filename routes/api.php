3414bb2e1104affbbced714116a30e613b42b1c7
menna-walid24 <mennawalid1244@gmail.com>
2026-08-13 20:05:13 +0300

    feat: add chatbot endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,17 @@
 
 use App\Http\Controllers\Auth\RegisteredUserDataController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatbotController;
 use App\Http\Controllers\BloodRequestController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/auth/me', RegisteredUserDataController::class)->name('auth.me');
-
-
-    Route::post('/blood-requests', [BloodRequestController::class, 'store'])->name('blood-requests.store');
-
-    Route::get('/blood-requests', [BloodRequestController::class, 'index'])->name('blood-requests.index');
-});
-
+Route::middleware(['auth:sanctum'])->get('/auth/me', RegisteredUserDataController::class)->name('auth.me');
+Route::POST('/blood-requests',[BloodRequestController::class,'store']) ->middleware('auth:sanctum');
 require __DIR__ . '/auth.php';
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API is working'
+    ]);
+});
+Route::middleware(['auth:sanctum'])->post('/chatbot',[ChatbotController::class,'sendMessage']);
+Route::middleware(['auth:sanctum'])->get('/chatbot/messages',[ChatbotController::class,'retrieveMessages']);
+
